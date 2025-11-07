547903fe55e86e05f3f022668b8d754de656ed54
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-11-07 16:24:45 +0200

    fix: update dependencies and correct utils

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -5,4 +5,20 @@ if (!PORT) {
   console.error('PORT is not defined in .env')
 }
 
-module.exports = { PORT }
+const MONGODB_URI = process.env.MONGODB_URI
+if (!MONGODB_URI) {
+  console.error('MONGODB_URI is not defined in .env')
+}
+
+const SECRET = process.env.SECRET
+if (!SECRET) {
+  console.error('SECRET is not defined in .env')
+}
+
+const ADMIN_PSW = process.env.ADMIN_PSW
+if (!ADMIN_PSW) {
+  console.error('ADMIN_PSW is not defined in .env')
+}
+
+
+module.exports = { PORT, MONGODB_URI, SECRET, ADMIN_PSW }
